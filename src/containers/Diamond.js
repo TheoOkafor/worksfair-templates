635696f6669d583fac3114470a4272a76a9ee671
Chukwuma Mary <petersbrenda47@gmail.com>
2020-07-07 21:55:15 +0100

    test changes

--- src/containers/Diamond.js
+++ src/containers/Diamond.js
@@ -1,27 +1,30 @@
-import React from "react";
-import Navbar from "../components/diamond/Navbar";
-import Hero from "../components/diamond/Hero";
+import React from 'react';
+import Navbar from '../components/diamond/Navbar';
+import Hero from '../components/diamond/Hero';
 // import "../components/corporate-cool/corporate-cool.scss";
-import "../components/diamond/diamond.scss";
-import About from "../components/diamond/About";
-import Team from "../components/diamond/Team";
-import Footer from "../components/diamond/Footer";
-import Services from "../components/diamond/Services";
-import { website } from "../utils";
-import Features from "./../components/diamond/Features";
-import Quote from "./../components/diamond/Quote";
+import '../components/diamond/diamond.scss';
+import About from '../components/diamond/About';
+import Team from '../components/diamond/Team';
+import Footer from '../components/diamond/Footer';
+import Services from '../components/diamond/Services';
+import { website } from '../utils';
+import Features from './../components/diamond/Features';
+import Quote from './../components/diamond/Quote';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const Diamond = () => {
   return (
     <>
-      <Navbar website={website} />
-      <Hero website={website} />
-      <About website={website} />
-      <Services website={website} />
-      <Features website={website} />
-      <Team website={website} />
-      <Quote website={website} />
-      <Footer website={website} />
+      <Router>
+        <Navbar website={website} />
+        <Hero website={website} />
+        <About website={website} />
+        <Services website={website} />
+        <Features website={website} />
+        <Team website={website} />
+        <Quote website={website} />
+        <Footer website={website} />
+      </Router>
     </>
   );
 };
